3d91c4b7665323b57fa5add7842eb425f33b3e3a
Snigdha Chatterjee <115936248+SnigdhaChatterjee@users.noreply.github.com>
2023-03-04 22:14:12 +0530

    Commit

---
+++ addition.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,c;
+    printf("Enter two no.:");
+    scanf("%d%d",&a,&b);
+    c=a+b;
+    printf("Sum is %d",c);
+    return 0;
+}
---
+++ calculator.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int a,b;
+    printf("Enter 1st number:");
+    scanf("%d",&a);
+    printf("Enter 2nd number:");
+    scanf("%d",&b);
+    printf("Sum=%d\n",a+b);
+    printf("Sub=%d\n",a-b);
+    printf("Multiplication=%d\n",a*b);
+    printf("Division=%d\n",a/b);
+    printf("Modulas=%d",a%b);
+    return 0;
+}
